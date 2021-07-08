d1d1ee644ade8d38a0bf455d0ddeeadf690a6682
phipham1112 <gpp.phipham2k@gmail.com>
2021-07-08 22:08:01 +0700

    done

--- index.js
+++ index.js
@@ -72,45 +72,139 @@ io.on('connection', (socket) => {
       console.log("arduino connected")
 
       //get ard status 
-      fanSocket.emit("get-status")
+     // fanSocket.emit("get-status")
       
       //
       io.emit("fan-connection", {
         connected : true
-      })
+      });
+      // fanSocket.on("timerem",(data) => {
+      //     console.log(data.timerem)
+      //     socket.emit("timerem",{
+      //       timerem: data.timerem
+      //     });
+      // });
       fanSocket.on("disconnect", (reason) => {
+        fanSocket.disconnect(true)
         fanSocket = null
+        console.log("ard off" + reason)
         io.emit("fan-connection", {
           connected : false
         })
       });
-
       
     } else {
       console.log("web controller connected")
-
+    
       // get the connection of fan to show on the UI
       if(fanSocket == null){
-        socket.emit("fan-connection", {
-          connected : false
-        })
-      } else {
+          socket.emit("fan-connection", {
+            connected : false
+          })
+      } 
+      else{
         socket.emit("fan-connection", {
           connected : true
         })
       }
     }
-
-    
     socket.on("fan-send-server-status", data => {
-      console.log("fan send: " + data)
+      //console.log("fan send: " + data)
       io.emit("server-send-status-of-fan", data)
+    })
+    //commadn from ui
+    
+
+   // get Status nodeMcu => web
+    socket.on("status-sensor-web",(data) => {
+      //console.log("Web send to sever: " + data)
+      if(fanSocket != null){
+        fanSocket.emit("status-sensor")
+
+        fanSocket.on("status-sensor-on",(data) => {
+          socket.emit("status-sensor-node-on")
+         // console.log("SenSor - on: ", data)
+        })
+        fanSocket.on("status-sensor-off",(data) => {
+          socket.emit("status-sensor-node-off")
+         // console.log("SenSor - off: ", data)
+        })
+        fanSocket.on("status-btn-on",(data) =>{
+          socket.emit("btn-node-on")
+          //console.log("btn-on: " + data)
+        })
+        fanSocket.on("status-btn-off",(data) =>{
+          socket.emit("btn-node-off")
+          //console.log("btn-off: " + data)
+        })
+      }
+    });
+    socket.on("turn-on-fan", () => {
+      //send command to ard
+     // console.log("turn-on-fan");
+      if(fanSocket != null){
+        fanSocket.emit("server-send-turn-on-fan",{
+          fanMode: true
+        });
+      }
+    })
+
+    socket.on("turn-off-fan", () => {
+      //send command to ard
+     // console.log("turn-off-fan");
+     if(fanSocket != null) 
+        fanSocket.emit("server-send-turn-off-fan",{
+        fanMode: true
+      });
+    })
+
+    socket.on("turn-on-sensor", () => {
+      //send command to ard
+     // console.log("turn-off-fan");
+     if(fanSocket != null)
+      fanSocket.emit("server-send-turn-on-sensor",{
+        sensorMode: true
+      });
+    })
+
+    socket.on("turn-off-sensor", () => {
+      //send command to ard
+     // console.log("turn-off-fan");
+     if(fanSocket != null)
+      fanSocket.emit("server-send-turn-off-sensor",{
+        sensorMode: true
+      });
+
+    })
+
+    socket.on("set-time-fan", (data) => {
+      if(fanSocket != null){
+        fanSocket.emit("set-time",{
+          time: data
+        });
+      }
+    });
+    // recive time from NodeMcu
+    if(fanSocket != null){
+      fanSocket.on("timerem",(data) => {
+        console.log("Revice node Mcu: " + data.timerem)
+        // Send time to Web //broadcast
+        io.emit("timerem-to-web", {
+          time: data.timerem
+        });
+      });
+     }
+    //commnad from arduino
+    socket.on("fan-send-server-status-fan", (data) => {
+      console.log(data)
+
+      //broadcash len all socket
+      io.emit("server-send-status-fan", data)
     })
 
     socket.on("disconnect", (reason) => {
         console.log(reason)
     });
   });
-
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -6,25 +6,13 @@ const timingBtn = document.getElementById("timing")
 const input = document.getElementById("input")
 const fanConnection = document.getElementById("fan-cn")
 
-btnOn.addEventListener("click", e => {
-    console.log("click")
-})
+sensorOn.classList.add("on-clicked")
+btnOn.classList.add("btn-disable")
+btnOff.classList.add("btn-disable")
+timingBtn.classList.add("btn-disable")
+input.classList.add("btn-disable")
 
-btnOff.addEventListener("click", e => {
-    console.log("click")
-})
-
-sensorOn.addEventListener("click", e => {
-    console.log("click")
-})
-
-sensorOff.addEventListener("click", e => {
-    console.log("click")
-})
-
-timingBtn.addEventListener("click", e => {
-    console.log("click" + input.value)
-})
+        
 
 
 //socket client
@@ -36,14 +24,150 @@ const socket = io({
 
 socket.on("connect", () => {
     console.log(socket.id); 
+
+    socket.emit("status-sensor-web")
+
+    socket.on("status-sensor-node-on",() => {
+        console.log("Sensor on")
+        sensorOn.classList.add("on-clicked")
+        btnOn.classList.add("btn-disable")
+        btnOff.classList.add("btn-disable")
+        timingBtn.classList.add("btn-disable")
+        input.classList.add("btn-disable")
+        
+        btnOn.classList.remove("on-clicked")
+        btnOff.classList.remove("off-clicked")
+        timingBtn.classList.remove("on-clicked")
+        sensorOff.classList.remove("off-clicked")
+    })
+    socket.on("status-sensor-node-off",() =>{
+        console.log("Sensor off")
+
+        btnOff.classList.add("off-clicked")
+        btnOn.classList.remove("on-clicked")
+        sensorOn.classList.remove("on-clicked")
+        sensorOff.classList.add("off-clicked")
+        timingBtn.classList.remove("on-clicked")
+
+
+        btnOn.classList.remove("btn-disable")
+        btnOff.classList.remove("btn-disable")
+        timingBtn.classList.remove("btn-disable")
+        input.classList.remove("btn-disable")
+
+    })
+    socket.on("btn-node-on", ()=>{
+        btnOn.classList.add("on-clicked")
+        btnOff.classList.remove("off-clicked")
+        sensorOff.classList.add("off-clicked")
+    })
+    socket.on("btn-node-off", ()=>{
+        btnOn.classList.remove("on-clicked")
+        btnOff.classList.add("off-clicked")
+        sensorOff.classList.add("off-clicked")
+    })
+
 });
 
+
+
 socket.on("fan-connection", data => {
+    console.log(data)
     if(data.connected){
         fanConnection.innerHTML = "Quạt đang online"
-        fanConnection.className += "online"
+        fanConnection.classList.remove("offline")
+        fanConnection.classList.remove("online")
+        fanConnection.classList.add("online")
     } else {
+        sensorOn.classList.add("on-clicked")
+        btnOn.classList.add("btn-disable")
+        btnOff.classList.add("btn-disable")
+        timingBtn.classList.add("btn-disable")
+        input.classList.add("btn-disable")
+
         fanConnection.innerHTML = "Quạt đang offline"
-        fanConnection.className += "offline"
+        fanConnection.classList.remove("offline")
+        fanConnection.classList.remove("online")
+        fanConnection.classList.add("offline")
+    }
+
+})
+
+socket.on("server-send-status-fan", data => {  //serevr gui qua
+    console.log(data)
+})
+
+
+/// event listener
+let checkTime = 0;
+btnOn.addEventListener("click", e => {
+    console.log("click btnOn")
+        //send command to srver 
+    btnOn.classList.add("on-clicked")
+    btnOff.classList.remove("off-clicked")
+    socket.emit("turn-on-fan")
+})
+
+btnOff.addEventListener("click", e => {
+  
+    console.log("click btnOff")
+    btnOn.classList.remove("on-clicked")
+    btnOff.classList.add("off-clicked")
+
+    timingBtn.classList.remove("on-clicked")
+    socket.emit("turn-off-fan")
+})
+
+sensorOn.addEventListener("click", e => {
+    console.log("click sensorOn")
+    //css
+        sensorOn.classList.add("on-clicked")
+        sensorOff.classList.remove("off-clicked")
+    
+        btnOn.classList.remove("on-clicked")
+        btnOff.classList.remove("off-clicked")
+        timingBtn.classList.remove("on-clicked")
+    
+        btnOn.classList.add("btn-disable")
+        btnOff.classList.add("btn-disable")
+        timingBtn.classList.add("btn-disable")
+        input.classList.add("btn-disable")
+        socket.emit("turn-on-sensor")
+})
+
+sensorOff.addEventListener("click", e => {
+    console.log("click sensorOff")
+    sensorOn.classList.remove("on-clicked")
+    sensorOff.classList.add("off-clicked")
+
+    btnOn.classList.remove("on-clicked")
+    btnOff.classList.add("off-clicked")
+    timingBtn.classList.remove("on-clicked")
+
+    btnOn.classList.remove("btn-disable")
+    btnOff.classList.remove("btn-disable")
+    timingBtn.classList.remove("btn-disable")
+    input.classList.remove("btn-disable")
+
+    socket.emit("turn-off-sensor")
+
+
+})
+
+timingBtn.addEventListener("click", e => {
+    console.log("click " + input.value)
+    timingBtn.classList.add("on-clicked")
+    btnOff.classList.remove("off-clicked")
+    btnOn.classList.add("on-clicked")
+    socket.emit("set-time-fan", input.value)
+})
+// recive from sever
+socket.on("timerem-to-web",(data) => {
+    console.log("timerem-to-web" + data)
+    if(parseInt(data.time) == 0){
+        timingBtn.classList.remove("on-clicked")
+        btnOff.classList.add("off-clicked")
+        btnOn.classList.remove("on-clicked")
     }
 })
+
